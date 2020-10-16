a21166fdcbd12624b3481e6cfdece3973c9c8764
anmarkosyan <anush.markos@gmail.com>
2020-10-16 19:10:20 +0400

    how to use events

---
+++ demo/events.js
@@ -0,0 +1,13 @@
+const EventEmitter = require('events');
+
+const emitter = new EventEmitter()
+
+emitter.on('anything', data => {
+    console.log('on: anything', data)
+})
+emitter.emit('anything', {a:1});
+emitter.emit('anything', {b:2});
+
+setTimeout(() => {
+    emitter.emit('anything', {c:3})
+}, 1500)
