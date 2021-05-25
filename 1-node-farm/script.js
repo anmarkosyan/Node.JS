6c892f96cb628b22c97aa4bb6271c0854ddaddad
anmarkosyan <anush.markos@gmail.com>
2021-05-25 21:39:02 +0400

    storing json data into global variable, parsing into object

--- 1-node-farm/script.js
+++ 1-node-farm/script.js
@@ -31,14 +31,20 @@ const url = require("url");
 //   });
 // });
 
-//HTTP
+//SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
-  //console.log(pathName);
+
   if (pathName === "/" || pathName === "/overview") {
     res.end("This is the OVERVIEW");
   } else if (pathName === "/product") {
     res.end("This is the Product");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
