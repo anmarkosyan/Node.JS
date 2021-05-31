5429d3183c769660b495cddfbb8c08af58f046e3
anmarkosyan <anush.markos@gmail.com>
2021-05-31 11:15:43 +0400

    url.parse is Deprecated!!!, so I changes it into new URL constructor object and get the same result, but more secure for users

--- 1-node-farm/script.js
+++ 1-node-farm/script.js
@@ -1,7 +1,9 @@
-//Module
+//Modules
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+
+const replaceTemplate = require('./modules/replaceTemplates');
 
 //File system
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
@@ -12,24 +14,11 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 //Functions and variables
 const dataArr = JSON.parse(data);
 
-const replaceTemplate = (template, product) => {
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%FROM%}/g, product.from);
-
-  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-
-  return output;
-};
-
 //SERVER
 const server = http.createServer((req, res) => {
-  const { query, pathname } = url.parse(req.url, true);
+  const baseURL = `https://${req.headers.host}/`;
+  const reqUrl = new URL(req.url, baseURL);
+  const { searchParams, pathname } = reqUrl;
 
   //overview page
   if (pathname === '/' || pathname === '/overview') {
@@ -43,7 +32,7 @@ const server = http.createServer((req, res) => {
   } else if (pathname === '/product') {
     res.writeHead(200, { 'Content-type': 'text/html' });
 
-    const product = dataArr[query.id];
+    const product = dataArr[searchParams.get('id')];
     const output = replaceTemplate(tempProduct, product);
 
     res.end(output);
