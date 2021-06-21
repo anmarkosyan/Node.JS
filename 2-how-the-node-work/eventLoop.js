836d958e88c66f91b686bf10955b986bb23f43c0
anmarkosyan <anush.markos@gmail.com>
2021-06-21 17:23:00 +0400

    how does work thread pool

--- 2-how-the-node-work/eventLoop.js
+++ 2-how-the-node-work/eventLoop.js
@@ -1,20 +1,36 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
+const start = new Date();
+
+process.env.UV_THREADPOOL_SIZE = '4';
 
 setTimeout(() => console.log('Timer 1 finish'), 0);
 setImmediate(() => console.log('Immediate finish'));
 
-process.nextTick(()=> console.log('Outside next tick '))
+process.nextTick(() => console.log('Outside next tick '));
 
 fs.readFile('test-file.txt', () => {
   console.log('I/O finished');
 
   setTimeout(() => console.log('Timer 1 inside file system'), 0);
   setTimeout(() => console.log('Timer 2 inside file system'), 1000);
-
   setImmediate(() => console.log('Immediate  inside file system'));
 
-  process.nextTick(()=> console.log('insideNext tick first'))
+  process.nextTick(() => console.log('insideNext tick first'));
+
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(new Date() - start,'password encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(new Date() - start,'password encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(new Date() - start,'password encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(new Date() - start,'password encrypted');
+  });
 });
 
 console.log('I am a first))))))))');
