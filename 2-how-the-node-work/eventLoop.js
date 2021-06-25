b5373b6ba4818b4a8535cce045fe12fafcb060db
anmarkosyan <anush.markos@gmail.com>
2021-06-25 17:55:06 +0400

    how to work with dynamic import modules

--- 2-how-the-node-work/eventLoop.js
+++ 2-how-the-node-work/eventLoop.js
@@ -2,35 +2,38 @@ const fs = require('fs');
 const crypto = require('crypto');
 
 const start = new Date();
+//========= testing dynamic modules ========
+module.exports.concat = (paramA, paramB) => paramA + paramB;
+//========
 
 //process.env.UV_THREADPOOL_SIZE = '8';// or can
 
-setTimeout(() => console.log('Timer 1 finish'), 0);
-setImmediate(() => console.log('Immediate finish'));
-
-process.nextTick(() => console.log('Outside next tick '));
-
-fs.readFile('test-file.txt', () => {
-  console.log('I/O finished');
-
-  setTimeout(() => console.log('Timer 1 inside file system'), 0);
-  setTimeout(() => console.log('Timer 2 inside file system'), 1000);
-  setImmediate(() => console.log('Immediate  inside file system'));
-
-  process.nextTick(() => console.log('insideNext tick first'));
-
-  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
-    console.log(new Date() - start,'password encrypted');
-  });
-  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
-    console.log(new Date() - start,'password encrypted');
-  });
-  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
-    console.log(new Date() - start,'password encrypted');
-  });
-  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
-    console.log(new Date() - start,'password encrypted');
-  });
-});
-
-console.log('I am a first))))))))');
+// setTimeout(() => console.log('Timer 1 finish'), 0);
+// setImmediate(() => console.log('Immediate finish'));
+//
+// process.nextTick(() => console.log('Outside next tick '));
+//
+// fs.readFile('test-file.txt', () => {
+//   console.log('I/O finished');
+//
+//   setTimeout(() => console.log('Timer 1 inside file system'), 0);
+//   setTimeout(() => console.log('Timer 2 inside file system'), 1000);
+//   setImmediate(() => console.log('Immediate  inside file system'));
+//
+//   process.nextTick(() => console.log('insideNext tick first'));
+//
+//   crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+//     console.log(new Date() - start,'password encrypted');
+//   });
+//   crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+//     console.log(new Date() - start,'password encrypted');
+//   });
+//   crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+//     console.log(new Date() - start,'password encrypted');
+//   });
+//   crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+//     console.log(new Date() - start,'password encrypted');
+//   });
+// });
+//
+// console.log('I am a first))))))))');
--- 2-how-the-node-work/events.js
+++ 2-how-the-node-work/events.js
@@ -1,5 +1,16 @@
 const EventEmitter = require('events');
 const http = require('http');
+//1️⃣static import
+// const exp = require('./eventLoop');
+// console.log(exp.concat(1, 3));
+
+//2️⃣ dynamic import
+const loadMyModule = async function () {
+  const { concat } = await import('./eventLoop.js');
+  console.log(concat(2, 3));
+};
+
+loadMyModule();
 
 //1️⃣ how to use event-driven architecture, EventEmitter global class,
 // for creating our own instances, and use them
@@ -27,24 +38,21 @@ const http = require('http');
 
 //2️⃣ how does work HTTP core API behind a sens, actually all core modules
 
-const server = http.createServer();
-
-server.on('request', (req, res) => {
-  console.log('request received');
-  res.end('request received');
-});
-
-server.on('request', (req, res) => {
-  console.log('Another request received');
-});
-
-server.on('close', () => {
-  console.log('server is closed');
-});
-
-server.listen(8000, '127.0.0.1', () => {
-  console.log('Listening to requests on port 8000...');
-});
-
-
-
+// const server = http.createServer();
+//
+// server.on('request', (req, res) => {
+//   console.log('request received');
+//   res.end('request received');
+// });
+//
+// server.on('request', (req, res) => {
+//   console.log('Another request received');
+// });
+//
+// server.on('close', () => {
+//   console.log('server is closed');
+// });
+//
+// server.listen(8000, '127.0.0.1', () => {
+//   console.log('Listening to requests on port 8000...');
+// });
